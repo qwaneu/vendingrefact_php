9ea67132985642ae093317fc77183aa4c0acae34
Willem van den Ende <willem@qwan.eu>
2024-09-06 11:00:01 +0100

    feat: Add VendingMachineGeneratedTest

---
+++ tests/VendingMachineGeneratedTest.php
@@ -0,0 +1,16 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+use VendingMachine;
+use Can;
+
+class VendingMachineGeneratedTest extends TestCase
+{
+    public function testChoiceless_machine_delivers_nothing() {
+      //  $this->assertEquals(Can.none, machine.deliver(Choice.cola));
+      //  assertEquals(Can.none, machine.deliver(Choice.fanta));
+    }
+
+
+}
